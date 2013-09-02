d150be995a97a16cd07767928b4c10c901075f34
vlad.iovanov <vlad.iovanov@uhurusoftware.com>
2013-09-02 14:23:51 +0300

    Bugfixes for mssql gateway.
    Change-Id: I253d0c0543c5c0ac5273cbbb7f4f016f55e244df

---
+++ src/mssql_service/lib/mssql_service/common.rb
@@ -0,0 +1,12 @@
+# Copyright (c) 2009-2011 VMware, Inc.
+module VCAP
+  module Services
+    module Mssql
+      module Common
+        def service_name
+          "MssqlaaS"
+        end
+      end
+    end
+  end
+end
